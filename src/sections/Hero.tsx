bfba39fe718ef053673925cf782ec55b34a1cb80
sudarshan1212 <sudarshansam33@gmail.com>
2024-09-17 22:51:22 +0530

    centered

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,10 +8,10 @@ export const Hero = () => {
         background: `url(${starbg.src})`,
       }}
     >
-      
+
       <div className="absolute h-64 w-64 bg-purple-500 rounded-full border-white/30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,60))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
       <div className="container relative">
-        <h1 className="text-8xl font-semibold tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,2,138,.5))] bg-white text-transparent bg-clip-text">
+        <h1 className="text-8xl font-semibold text-center tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,2,138,.5))] bg-white text-transparent bg-clip-text">
           AI SEO
         </h1>
         <p className="text-lg text-white/70 mt-5 text-center">
